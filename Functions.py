b8ec8463f27849f1aa6378f00d0ac7dd59610ae3
Raven <RavenNHWelch@gmail.com>
2020-08-18 11:22:51 -0400

    Updated functions file. Added test_models, stacked_model, resample, and resample_skip_neg

--- Functions.py
+++ Functions.py
@@ -1,24 +1,195 @@
 import pandas as pd
 import numpy as np 
-from sklearn.model_selection import train_test_split
 from tqdm import tqdm 
+from pprint import pprint
+import enchant
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.utils import resample
+
+
 import nltk 
 from nltk.stem import WordNetLemmatizer 
 from nltk.tokenize import RegexpTokenizer, sent_tokenize, word_tokenize, TweetTokenizer
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-import enchant
-from pprint import pprint
-import pickle
 
-def import_tweet_data():
-    """Imports the tweet data from the 'data' folder, 
-    with ISO-8859-1 encoding.
+def test_models(models, n_jobs = 2):
+    """
+    Test all models given.
     
-    Output: A Pandas DataFrame"""
+    Hey sam I would appriciate it if you could add to this doc string!"""
+    results = []
+    model_names = []
+    pbar = tqdm(models.items())
     
-    df = pd.read_csv('data/TweetsOriginal.csv', encoding = 'ISO-8859-1' )
-    return df
+    for model, m in pbar: 
+        pbar.set_description(f'Evaluating {model.upper()}')
+        cv = RepeatedStratifiedKFold(n_splits = 5, n_repeats = 5)
+        scores = cross_val_score(m, x_train_new, y_train_new, scoring = 'accuracy', cv = cv, n_jobs = n_jobs, 
+                                 error_score = 'raise')
+        results.append(scores)
+        model_names.append(model)
+        
+    return results, model_names
+
+def stacked_model(models):
+    """Creates a stacked model given a dictionary of SciKitLearn models
+    -----------------------------------------
+    Input: 
+        models: Dictionary containing the model name and function.
+    
+    Output: 
+        stack_model: A SciKitLearn StackingClassifier object
+    -----------------------------------------"""
+
+    stack_m = [] 
+    for model, m in models.items(): 
+        stack_m.append((model, m))
+    stack_model = StackingClassifier(estimators = stack_m, final_estimator = LogisticRegression(), cv = 5)
+    models['stacked'] = stack_model
+    
+    return stack_model
+
+def resample(x_train, y_train, num_targets, n_samples):
+    
+    """Allows resampling of a dataframe as long as there are 2 or 3 targets. 
+    Resamples all targets.
+    
+    Input: 
+    
+    x_train: array of features
+    y_train: 1D array of targets
+    num_targets: the number of targets, 2 or 3
+    n_samples: how many samples will be chosen
+    
+    Output: 
+    X_train: A pandas DataFrame
+    y_train: A numpy array object
+    """
+    
+    print(f"""Original Train Value Counts
+{y_train.value_counts()}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Original Test Value Counts
+{y_test.value_counts()}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+
+    train_df = pd.DataFrame(x_train)
+    train_df['target'] = y_train.values 
+    
+    neg_df = train_df[train_df.target == 0]
+    pos_df = train_df[train_df.target == 1]
+
+    resample_pos = resample(pos_df, n_samples = n_samples, random_state = 10, replace = False)
+    resample_neg = resample(neg_df, n_samples = n_samples, random_state = 10, replace = False)
+    
+    df = resample_neg.append(resample_pos, ignore_index = True)
+    
+    if num_targets == 3:
+        neut_df = train_df[train_df.target == 2]
+        resample_neut = resample(neut_df, n_samples = n_samples, random_state = 10, replace = False)
+        df = df.append(resample_neut, ignore_index = True)
+    
+    X_train = df.drop(columns = 'target')
+    y_train = df.target.values.ravel()
+    
+    print(f"""Final Resampled Value Counts
+{final_df.target.value_counts()}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+    
+    return X_train, y_train
+
+
+def resample_skip_neg(x_train, y_train, num_targets, n_samples):
+    
+        """Allows resampling of a dataframe as long as there are 2 or 3 targets. 
+    Resamples all targets except for those with a "0", the negative sentiment
+    value. 
+    
+    Input: 
+    
+    x_train: array of features
+    y_train: 1D array of targets
+    num_targets: the number of targets, 2 or 3
+    n_samples: how many samples will be chosen
+    
+    Output: 
+    X_train: A pandas DataFrame
+    y_train: A numpy array object
+    """
+    
+    print(f"""Original Train Value Counts
+{y_train.value_counts()}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Original Test Value Counts
+{y_test.value_counts()}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+
+    train_df = pd.DataFrame(x_train)
+    train_df['target'] = y_train.values 
+    
+    neg_df = train_df[train_df.target == 0]
+    pos_df = train_df[train_df.target == 1]
+
+    resample_pos = resample(pos_df, n_samples = n_samples, random_state = 10, replace = False)
+    
+    df = neg_df.append(resample_neg, ignore_index = True)
+    
+    if num_targets == 3:
+        neut_df = train_df[train_df.target == 2]
+        resample_neut = resample(neut_df, n_samples = n_samples, random_state = 10, replace = False)
+        df = df.append(resample_neut, ignore_index = True)
+    
+    X_train = df.drop(columns = 'target')
+    y_train = df.target.values.ravel()
+    
+    print(f"""Final Resampled Value Counts
+{final_df.target.value_counts()}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+    
+    return X_train, y_train
+
+    
+def get_pickles(split_type = 0): 
+    
+    """
+    Opens up the training and test data files. 
+    
+    Input:
+    
+    split_type: int. the split type of the file, based on if there are
+    2 or 3 classes that are being used for prediction. 
+    """
+    
+    if split_type > 0:
+        split_type = str(split_type) + '_'
+        
+    x_train = pickle.load(open(f'Pickles/{split_type}x_train.p', 'rb'))
+    x_test = pickle.load(open(f'Pickles/{split_type}x_test.p', 'rb'))
+    y_train = pickle.load(open(f'Pickles/{split_type}y_train.p', 'rb'))
+    y_test = pickle.load(open(f'Pickles/{split_type}y_test.p', 'rb'))
+    return x_train, x_test, y_train, y_test
+
+def save_cv_results(model_names, results, filename):
+    """
+    Pickles the model's results
+    
+    Input: 
+    
+    model_names: list of model names 
+    results: list of results 
+    filename: str, path for the file to be saved
+    
+    Output: 
+    
+    Pickle file, saved in the location specified in filename
+    """
+    vanilla_dict = {i:y for i,y in zip(model_names, results)}
+    return pickle.dump(vanilla_dict, open(filename, 'wb'))
+
+
 
 def encode_emotion_3(x): 
     x = x.lower() 
@@ -41,7 +212,6 @@ def encode_emotion_2(x):
     else: 
         return None
     
-
 
 def clean_split(split_type, df): 
     new_df = pd.DataFrame() 
